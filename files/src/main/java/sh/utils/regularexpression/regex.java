d9bdbc32cd98b780cadd4f2414461de62c91e0dd
sheom <sheom@DESKTOP-OA0K8S3>
2020-08-14 15:46:02 +0900

    정규표현식 사전 로드방식 변경

--- files/src/main/java/sh/utils/regularexpression/regex.java
+++ files/src/main/java/sh/utils/regularexpression/regex.java
@@ -1,18 +1,59 @@
 package sh.utils.regularexpression;
 
-import java.io.IOException;
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class regex {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
+	private final List<regexModel> regexPrintList = new ArrayList<regexModel>();
+	private final List<regexModel> regexRemoveList = new ArrayList<regexModel>();
+
+	/**
+	 * @Author SH
+	 * @param
+	 * @return 졍규표현식 사전 로드
+	 */
+	public regex() {
+		try {
+			File printFile = null;
+			URL printResource = regex.class.getResource("/regex_print.txt");
+			printFile = Paths.get(printResource.toURI()).toFile();
+			List<String> printList = FileUtils.readLines(printFile, "UTF-8");
+			for (String reg : printList) {
+				Pattern pattern = Pattern.compile(reg);
+				regexPrintList.add(new regexModel(pattern));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+
+			File removeFile = null;
+			URL removeResource = regex.class.getResource("/regex_remove.txt");
+				removeFile = Paths.get(removeResource.toURI()).toFile();
+			List<String> removeLlist = FileUtils.readLines(removeFile, "UTF-8");
+			for (String reg : removeLlist) {
+				Pattern pattern = Pattern.compile(reg);
+				regexRemoveList.add(new regexModel(pattern));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	/**
 	 * @Author SH
 	 * @param 데이터
@@ -20,25 +61,18 @@ public class regex {
 	 */
 	public String removeRegex(String data) {
 		logger.info(data);
-		String txt;
-		try {
-			txt = IOUtils.toString(getClass().getResourceAsStream("/regex_remove.txt"), "UTF-8");
-		} catch (IOException e) {
-			e.printStackTrace();
-			return data;
-		}
-		String rgx[] = txt.split("\n");
-		for (String rg : rgx) {
-			Pattern Script = Pattern.compile(rg);
-			Matcher Mat = Script.matcher(data);
 
-			while (Mat.find())
-				data = data.replaceAll(Mat.group(), "");
+		for(regexModel regex : regexPrintList) {
+			Matcher matcher = regex.getPattern().matcher(data);
+			while (matcher.find())
+				data = data.replaceAll(matcher.group(), "");
 			
 			logger.info(data);
 		}
+		
 		return data;
 	}
+
 	/**
 	 * @Author SH
 	 * @param 데이터
@@ -47,23 +81,15 @@ public class regex {
 	public List<String> printRegex(String data) {
 		List<String> list = new ArrayList<String>();
 		logger.info(data);
-		String txt;
-		try {
-			txt = IOUtils.toString(getClass().getResourceAsStream("/regex_print.txt"), "UTF-8");
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-		String rgx[] = txt.split("\n");
-		for (String rg : rgx) {
-			Pattern Script = Pattern.compile(rg);
-			Matcher Mat = Script.matcher(data);
-
-			while (Mat.find()) {
-				list.add(Mat.group());
-				logger.info(Mat.group());
+		
+		for(regexModel regex : regexPrintList) {
+			Matcher matcher = regex.getPattern().matcher(data);
+			while (matcher.find()) {
+				list.add(matcher.group());
+				logger.info(matcher.group());
 			}
 		}
+		
 		return list;
 	}
 
